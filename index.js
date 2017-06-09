070d1b4ef4d3b7a9c2628b8e715de02cd4f14a47
Leon Wu <lwgithub@gmail.com>
2017-06-08 21:56:57 -0400

    Show placement of mines

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-
-
 // game parameters
 var cellsPerRow = 6;
 var cellsPerCol = 6;
 var numMines = 5; // todo: put in a check so that the number of mines doesn't exceed the number of cells.
-
-// generate random locations for mines.
 
 
 // Put together game board.
@@ -15,14 +11,27 @@ for (var row = 0; row < cellsPerRow; row++) {
     for (var col = 0; col < cellsPerCol; col++) {
         var button = document.createElement('button');
         button.id = 'button' + row + col;
+        // TODO: refactor this from trying to manipulate strings and into some more coherent pattern, such as an object with a row and column or something. See all locations tagged with the word COORDINATE.
         button.typeName = 'button';
         button.innerHTML = '.';
         button.className = 'button col-xs-' + 12 / cellsPerCol;
         button.onclick = function() {
             console.log(this.id);
-            // TODO: refactor this from trying to manipulate strings and into some more coherent pattern, such as an object with a row and column or something.
         }
         divRow.appendChild(button);   
     }
     document.getElementById('grid').appendChild(divRow);
 }
+
+// generate random locations for mines.
+var mineLocations = [];
+for (var i = 0; i < numMines; i++){
+    var row = Math.floor(Math.random() * cellsPerRow);
+    var col = Math.floor(Math.random() * cellsPerCol);
+    mineLocations.push([row, col]);
+}
+
+mineLocations.forEach(function(item, index, array) {
+    document.getElementById('button' + item[0] + item[1]).innerHTML = '*'; // TODO COORDINATE 
+    console.log(item, index);
+});
