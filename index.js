044ea7010f4964ea4a3ca1b2afc9a7a495ecf819
Leon Wu <lwgithub@gmail.com>
2017-06-08 21:31:46 -0400

    Initial array of clickable buttons

---
+++ index.js
@@ -0,0 +1,19 @@
+var cellsPerRow = 6;
+var cellsPerCol = 6;
+
+for (var row = 0; row < cellsPerRow; row++) {
+    var divRow = document.createElement('div');
+    divRow.className = 'row';
+    for (var col = 0; col < cellsPerCol; col++) {
+        var button = document.createElement('button');
+        button.id = 'button' + row + col;
+        button.typeName = 'button';
+        button.innerHTML = '.';
+        button.className = 'button col-xs-' + 12 / cellsPerCol;
+        button.onclick = function() {
+            console.log(this.id);
+        }
+        divRow.appendChild(button);   
+    }
+    document.getElementById('grid').appendChild(divRow);
+}
