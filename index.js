85356cc1a5b6d639992002b987bbd6f86642e84d
Leon Wu <lwgithub@gmail.com>
2017-06-17 22:56:38 -0400

    Change bad wording from cellsPerRow to numRows. Also for columns.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ jsDisabledMessage.parentNode.removeChild(jsDisabledMessage);
 // TODO: this entire program is run regarless of whether javascript is allowed when viewing this in chrome or not. not sure why; commenting it out prevents it from being run, so probably an issue with chrome settings or whatever, not js.
 
 // game parameters
-var cellsPerRow = 6;
-var cellsPerCol = 6;
+var numRows = 6;
+var numCols = 6;
 var numMines = 5; // todo: put in a check so that the number of mines doesn't exceed the number of cells.
 
 function cell(row, col) {
@@ -15,16 +15,16 @@ function cell(row, col) {
 }
 
 // Put together game board.
-for (var row = 0; row < cellsPerRow; row++) {
+for (var row = 0; row < numRows; row++) {
     var divRow = document.createElement('div');
     divRow.className = 'row';
-    for (var col = 0; col < cellsPerCol; col++) {
+    for (var col = 0; col < numCols; col++) {
         var button = document.createElement('button');
         button.id = 'button' + row + col;
         // TODO: refactor this from trying to manipulate strings and into some more coherent pattern, such as an object with a row and column or something. See all locations tagged with the word COORDINATE.
         button.typeName = 'button';
         button.innerHTML = '.';
-        button.className = 'button col-xs-' + 12 / cellsPerCol;
+        button.className = 'button col-xs-' + 12 / numCols;
         button.onclick = function() {
             console.log(this.id);
         }
@@ -36,8 +36,8 @@ for (var row = 0; row < cellsPerRow; row++) {
 // generate random locations for mines.
 var mineLocations = [];
 for (var i = 0; i < numMines; i++){
-    var row = Math.floor(Math.random() * cellsPerRow);
-    var col = Math.floor(Math.random() * cellsPerCol);
+    var row = Math.floor(Math.random() * numRows);
+    var col = Math.floor(Math.random() * numCols);
     mineLocations.push(new cell(row, col));
 }
 
@@ -47,7 +47,7 @@ function generateGameBoard() {
         document.getElementById('button' + mine.row + mine.col).innerHTML = '*';
         console.log(mine, index);
     });
-    
+
 }
 
 generateGameBoard();
