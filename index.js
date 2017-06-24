efd49df17a74f2a8d04b928df4a6188bfcffceb4
Leon Wu <lwgithub@gmail.com>
2017-06-24 17:16:25 -0400

    Add todo to generateGameBoard

--- index.js
+++ index.js
@@ -34,14 +34,6 @@ for (var row = 0; row < numRows; row++) {
     document.getElementById('grid').appendChild(divRow);
 }
 
-// generate random locations for mines.
-var mineLocations = [];
-for (var i = 0; i < numMines; i++){
-    var row = Math.floor(Math.random() * numRows);
-    var col = Math.floor(Math.random() * numCols);
-    mineLocations.push(new cell(row, col));
-}
-
 function numAdjacent(row, col, mineLocations) {
     // returns the number of mines adjacent to the cell with given row and column.
     // mineLocations is an array of cells that are the locations of the mines.
@@ -55,23 +47,59 @@ function numAdjacent(row, col, mineLocations) {
     return adj;
 }
 
+/*
+Constructs game board given numRows, numCols, and numMines. Returns 2-D array representing the board state of dimensions numRows x numCols.
+
+todo: refactor this to take parameters instead of relying on global variables numRows, numCols, and numMines.
+*/
 function generateGameBoard() {
-    // place mines
-    mineLocations.forEach(function(mine, index, array) {
-        document.getElementById('button' + mine.row + mine.col).innerHTML = '*';
-        console.log(mine, index);
-    });
+    // var gameBoard = Array(numRows).fill(Array(numCols).fill(0)); // placeholder to make everything zero at first.
+
+    var gameBoard = [];
+    for (var row = 0; row < numRows; row++) {
+        var rowArray = [];
+        for (var col = 0; col < numCols; col++) {
+            rowArray.push(0);
+        }
+        gameBoard.push(rowArray);
+    }
+    /** gameBoard[row][col] is 2-D int array representing the status of the cells. If a given element is -1, then there is a mine at that location; otherwise, the number is the number of mines around it. */
+
+    // generate random locations for mines.
+    var mineLocations = []; // handy list of mine coordinates for convenience. Can be derived from gameBoard but this is more convenient.
+    for (var i = 0; i < numMines; i++){
+        var row = Math.floor(Math.random() * numRows);
+        var col = Math.floor(Math.random() * numCols);
+        gameBoard[row][col] = -1; // -1 indicates is a mine.
+        mineLocations.push(new cell(row, col));
+    }
 
     // assign numbers to non-numerical cells
     for (var row = 0; row < numRows; row++) {
         for (var col = 0; col < numCols; col++) {
-            if (!mineLocations.find((mineLocation, index, array) => {
-                return mineLocation.isEqual(new cell(row, col)); })) {
-                // If the given cell is not found within mineLocations, then that cell doesn't have a mine.
-                document.getElementById('button' + row + col).innerHTML = numAdjacent(row, col, mineLocations);
+            if (gameBoard[row][col] != -1) {
+                // If the given cell doesn't have a mine, then we need the number of mines around it.
+                // console.log(gameBoard[row][col]);
+                gameBoard[row][col] = numAdjacent(row, col, mineLocations);
             }
         }
     }
+
+    // NOTE TO FUTURE SELF-- YOU CAN'T PUT THIS IN THE PREVIOUS LOOP.
+    // because for any given cell, the entire board might not be fully set up yet so assigning numbers could give bad results.
+    for (var row = 0; row < numRows; row++) {
+        for (var col = 0; col < numCols; col++) {
+            var symbol;
+            if (gameBoard[row][col] != -1) {
+                symbol = '' + gameBoard[row][col];
+            }
+            else {
+                symbol = "*"; // for mines
+            }
+            document.getElementById('button' + row + col).innerHTML = symbol;
+        }
+    }
+    return gameBoard;
 }
 
 generateGameBoard();
