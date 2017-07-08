88bc2770a4972ecdf1d3562cd4db0abc0d9c4212
Leon Wu <lwgithub@gmail.com>
2017-07-07 21:31:43 -0400

    update todo comments

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 
 /*
 Other assorted things (no set timeline)
-- things marked PROD, aka things to be done when no longer in debug mode.
 - refactor button.id (anything labeled COORDINATE). See comments there to see--basically try to avoid manipulating strings for everything.
 - this entire program is run regarless of whether javascript is allowed when viewing this in chrome or not. not sure why; commenting it out prevents it from being run, so probably an issue with chrome settings or whatever, not js.
 - add a game timer
@@ -13,6 +12,7 @@ Other assorted things (no set timeline)
 - support chording--look up minesweeper wiki for this.
 - make the website independent of browser, version, mobile vs desktop, etc.
 - see link: http://www.chezpoor.com/minesweeper/help/minetechnicalnotes.html
+- things marked PROD, aka things to be done when no longer in debug mode. this includes some graphical changes, but also importantly-- hiding the numbers/mines for the cells-- in-game the player won't have access to that
 
 - overall plan-- implement logic => design/UI/UX => github pages => user testing.
 */
